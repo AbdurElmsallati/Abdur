88414d5b468752046d4fd150df7c51957bd578c6
AbdurElmsallati <91126388+AbdurElmsallati@users.noreply.github.com>
2021-10-11 09:03:24 +0100

    Update 11/10/21

---
+++ JS Products S.js
@@ -0,0 +1,25 @@
+ // Dog products //
+  var petProduct = {
+      dog1: {
+    breed : "BullDog",
+    desc : "Small but bulky",
+    image : "",
+    price : 9999
+
+  },
+
+    dog2: {
+  breed : "Golden Retriever",
+  desc : "A very good hunting dog",
+  image : "",
+  price : 45
+
+},
+
+    dog3: {
+  breed : "German Shepherd",
+  desc : "Amazing guard dog",
+  image : "",
+  price : 35
+    }
+};
