fbb3de253670e8bede7b0ab2dc53dc1293b7b3cb
AbdurElmsallati <91126388+AbdurElmsallati@users.noreply.github.com>
2021-10-15 11:20:01 +0100

    u

--- cart/main.js
+++ cart/main.js
@@ -96,12 +96,12 @@ function setItems(product) {
 function totalCost (product){ 
      console.log("The price of the dog", product.price); 
      let cartCost = localStorage.getItem('totalCost');
-      cartCost = parseInt(cartCost);
+      
       console.log("My cartCost is", cartCost);
       console.log(typeof cartCost );
 
     if(cartCost != null){
-     
+     cartCost = parseInt(cartCost);
      localStorage.setItem("totalCost", cartCost + product.price);
     } else {
         localStorage.setItem("totalCost", product.price);
